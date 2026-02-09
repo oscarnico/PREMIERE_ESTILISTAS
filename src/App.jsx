72be7756b06282abe0230effbcef3936636c94db
Oscar Nico <oscar@peanutbutter.es>
2026-02-09 15:23:40 +0100

    Initial commit: cleaned project with placeholder message

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Próximamente web Premiere Estilistas</h1>
+      <p>¡Estamos trabajando para ti! 💈</p>
+    </div>
+  );
+}
+
+export default App;
